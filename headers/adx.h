01a36a917c3603e9427daa6db5c46b0d6cc55194
adityaxprakash <cs1221595@cse.iitd.ac.in>
2024-02-09 21:28:55 +0530

    ADX strat

---
+++ headers/adx.h
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class adx
+{
+private:
+    string start_date;
+    string end_date;
+    int n;
+    double x;
+    double adx_threshhold;
+    vector<double> adx_arr={0};
+
+    ofstream cashfile;
+    ofstream statfile;
+    ofstream pandlfile;
+    vector<string> dates = {""};
+    vector<double> high = {0};
+    vector<double> low = {0};
+    vector<double> prices = {0};
+    vector<double> prev_close = {0};
+
+    int position = 0;
+    double cashflow = 0;
+
+    void calculate_adx();
+    void simulate_trades();
+    void write_daily_flow(string date, double cashflow);
+    void write_orders(string date, string action, string quantity, double price);
+    double calculate_ewm(int k, vector<double> &base, int idx);
+
+public:
+    adx(string start, string end, double x_, int n_, double threshhold);
+    void run(string infile, string cashflow_file, string order_stats_file, string pandl_file);
+};
