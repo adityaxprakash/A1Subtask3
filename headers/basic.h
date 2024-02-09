6a689f6b99c4aea780feba01e842ec61a9c72b77
AnshAgrawal04 <anshdbst04@gmail.com>
2024-02-09 22:39:03 +0530

    .\cpp\basic.cpp
    Basic Done

---
+++ headers/basic.h
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class basic
+{
+private:
+    string start_date;
+    string end_date;
+    int n;
+    double x;
+    ofstream cashfile;
+    ofstream statfile;
+    ofstream pandlfile;
+    vector<string> dates = {""};
+    vector<double> prices = {0};
+    vector<int> last_n={0};
+    
+    int position = 0;
+    double cashflow = 0;
+
+    void calculate_basic();
+    void simulate_trades();
+    void write_daily_flow(string date, double cashflow);
+    void write_orders(string date, string action, string quantity, double price);
+
+public:
+    basic(string start, string end, int n_, double x_);
+    void run(string infile, string cashflow_file, string order_stats_file, string pandl_file);
+};
