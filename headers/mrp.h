0027a415a29f4aa1b4714cc7010e4c121ad490df
AnshAgrawal04 <anshdbst04@gmail.com>
2024-02-10 02:58:15 +0530

    MRP strat

---
+++ headers/mrp.h
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class mrp{
+    private:
+        string start_date;
+        string end_date;
+        string stock1;
+        string stock2;
+        int n;
+        double x;
+        double mrp_threshold;
+        vector<double> adx_arr={0};
+
+        ofstream cashfile;
+        ofstream statfile;
+        ofstream pandlfile;
+
+        vector<string> dates = {""};
+        vector<double> stock1_prices={0};
+        vector<double> stock2_prices={0};
+        vector<double> spread={0};
+        vector<double> rolling_mean={0};
+        vector<double> rolling_std={0};
+        vector<double> sum={0};
+        vector<double> square_sum={0};
+        vector<double> z_score={0}; 
+        int position = 0;
+        double cashflow = 0;
+
+        void calculate_mrp();
+        void simulate_trades(string cashflow_file, string order_stats_file, string pandl_file);
+        void write_daily_flow(string date, double cashflow);
+        void write_orders(string date, string action, string quantity, double price,string stock);
+        
+    public:
+        mrp(string start, string end, double x_, int n_, double ,string stock1_,string stock2_);
+        void run(string infile1,string infile2, string cashflow_file, string order_stats_file, string pandl_file);    
+};
