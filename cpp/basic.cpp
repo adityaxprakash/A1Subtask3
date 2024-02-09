82434dff14942587a99f0287f14bf2c9f03640aa
AnshAgrawal04 <anshdbst04@gmail.com>
2024-02-09 22:41:03 +0530

    Basic done

--- cpp/basic.cpp
+++ cpp/basic.cpp
@@ -1,103 +1,146 @@
-#include <bits/stdc++.h>
+#include "../headers/basic.h"
 
-using namespace std;
-
-class basic
+basic::basic(string start, string end, int n_, double x_)
 {
-    int n;
-    int x;
-    string start;
-    string end;
+    this->start_date = start;
+    this->end_date = end;
+    this->n = n_;
+    this->x = x_;
+}
 
-public:
-    basic(int n, int x, string start, string end)
+void basic::calculate_basic()
+{
+    int num_days = dates.size();
+    int lastinc = 0;   
+    int lastdec = 0; 
+    double lastday = 0; 
+    int netstocks = 0;
+    for (int i = 0; i < num_days; i++)
     {
-        this->n = n;
-        this->x = x;
-        this->start = start;
-        this->end = end;
-    }
-    void run(string filename, string cashname, string statname)
-    {
-        int date_counter = -1;
-        int position = 0;
-        std::ifstream file(filename);
-        std::string line;
-        int lastdec = 0;
-        int lastinc = 0;
-        double lastday = 0;
-        double accumul = 0;
-        string closval;
-        int date;
-        int netstocks = 0;
-        std::ofstream cashfile(cashname);
-        std::ofstream statfile(statname);
-        cashfile << "Date,Cashflow\n";
-        statfile << "Date,Action,Quantity,Price,Value\n";
-        double profit = 0;
-        while (std::getline(file, line))
+        if (i == 0)
         {
-            if (date_counter == -1)
+            lastday = prices[i];
+        }
+        else if (i > 0)
+        {
+            if (prices[i] > lastday)
             {
-                date_counter++;
+                lastinc = i;
+            }
+            else if (prices[i] < lastday)
+            {
+                lastdec = i;
+            }
+            else{
+                lastinc = i;
+                lastdec = i;
+            }
+            lastday = prices[i];
+        }   
+        if (i > n - 1)
+        {
+            if (i -lastinc >n-1){
+                last_n.push_back(-1);
+            }
+            else if (i -lastdec >n-1){
+                last_n.push_back(1);
+            }
+            else{
+                last_n.push_back(0);
+            }
+        } 
+    }
+}
+
+void basic::write_daily_flow(string date, double cashflow)
+{
+    string to_write = date + " " + to_string(cashflow) + "\n";
+    cashfile << to_write;
+}
+
+void basic::write_orders(string date, string action, string quantity, double price)
+{
+    string to_write = date + "," + action + "," + quantity + "," + to_string(price) + "\n";
+    statfile << to_write;
+}
+void basic::simulate_trades()
+{
+    int num_days = dates.size();
+    cashflow = 0;
+    position = 0;
+    for (int i = 0; i < num_days; i++)
+    {
+        if (last_n[i] == -1)
+        {
+            if (position >-x)
+            {
+                position -= 1;
+                cashflow += prices[i];
+                write_orders(dates[i], "SELL", "1", prices[i]);
+            }
+        }
+        else if (last_n[i] == 1)
+        {
+            if (position < x)
+            {
+                position += 1;
+                cashflow -= prices[i];
+                write_orders(dates[i], "BUY", "1", prices[i]);
+            }
+        }
+        write_daily_flow(dates[i], cashflow);
+    }
+    double square_off = position * prices.back();
+    string p_and_l = to_string(square_off + cashflow);
+    pandlfile << "Basic" << "," << p_and_l << "\n";
+}
+
+
+void basic::run(string infile, string cashflow_file, string order_stats_file, string pandl_file){
+    ifstream file(infile);
+    cashfile.open(cashflow_file);
+    statfile.open(order_stats_file);
+    pandlfile.open(pandl_file);
+    cashfile << "Date,Cashflow\n";
+    statfile << "Date,Order_dir,Quantity,Price\n";
+
+    if (!file.is_open() || !statfile.is_open() || !cashfile.is_open() || !pandlfile.is_open())
+    {
+        cout << "Error: couldn't open" << '\n';
+    }
+
+    int line_number = 0;
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string date, price;
+        line_number++;
+        if (getline(ss, date, ',') && getline(ss, price, ','))
+        {
+            if (line_number == 1)
+            {
                 continue;
             }
-            
-            std::stringstream ss(line);
-            std::string date, closval;
-            if (getline(ss, date, ',') && getline(ss, closval, ','))
-            {
-                // cashfile << date << ","<<closval<<"\n";
-                if (date_counter == 0)
-                {
-                    lastday = std::stod(closval);
-                }
-                else if (date_counter > 0)
-                {
-                    if (std::stod(closval) > lastday)
-                    {
-                        lastinc = date_counter;
-                    }
-                    if (std::stod(closval) < lastday)
-                    {
-                        lastdec = date_counter;
-                    }
-                    lastday = std::stod(closval);
-                }
-
-                if (date_counter > n - 1)
-                {
-                    if (date_counter - lastinc > n-1)
-                    {
-                        if (accumul + stod(closval) < x)
-                        {
-                            netstocks -= 1;
-                            accumul += std::stod(closval);
-                            statfile << date << ","
-                                     << "SELL"
-                                     << "," << 1 << "," << closval << "\n";
-                        }
-                    }
-                    else if (date_counter - lastdec > n-1)
-                    {
-                        if (accumul - stod(closval) >-x)
-                        {
-                            netstocks += 1;
-                            accumul -= std::stod(closval);
-                            statfile << date << ","
-                                     << "BUY"
-                                     << "," << 1 << "," << closval << "\n";
-                        }
-                    }
-                    cashfile << date << "," << accumul << "\n";
-                    if (file.peek()==EOF){
-                        profit = netstocks * std::stod(closval) + accumul;
-                    }
-                }
-            }
-            date_counter++;
+            prices.push_back(stod(price));
+            dates.push_back(date);
         }
-        ofstream finalfile("final_pl.csv");
-        finalfile << "Basic"<<","<<profit<<","<<"\n";
     }
-};
+    //cout << "Read " << prices[10] << " prices" << '\n';
+    calculate_basic();
+
+    simulate_trades();
+
+    file.close();
+    statfile.close();
+    cashfile.close();
+    pandlfile.close();
+}
+
+
+
+
+
+
+
+
